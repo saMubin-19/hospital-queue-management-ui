6b949f8ad2cabd53c2b4d5f341acc31ab0800f80
saMubin-19 <almubin1903@gmail.com>
2026-01-08 21:25:35 +0600

    Initial frontend structure and CSS for Hospital Queue Management

---
+++ js/data.js
@@ -0,0 +1,4 @@
+const doctors = [
+  { name: "Dr. Sarah Ahmed", dept: "Cardiology", token: "T-045", wait: "45 mins" },
+  { name: "Dr. Kamal Hassan", dept: "Neurology", token: "T-032", wait: "30 mins" }
+];
